e67b5c6c65770d0b5a33011d75d0a474072e775e
Juan Oicata <jeotoicata@gmail.com>
2025-09-06 04:07:07 -0500

    intento de iniciar sesion 9, error en los nombres del html

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.sendFile('index.html', { root: './src/public' });
+    res.sendFile('index.html', { root: './public' });
 });
 
 export default router;
